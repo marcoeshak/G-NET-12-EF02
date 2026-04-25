a29d954bf39f5819f5d4b02eea109784aba35def
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 00:51:07 +0300

    Badge

---
+++ G-NET-12-EF02/Badge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+    public class Badge
+    {
+        public int Id { get; set; }
+
+        public string BadgeNumber { get; set; }
+        public DateTime IssueDate { get; set; }
+
+        public BadgeTier Tier { get; set; }
+
+        public int AttendeeId { get; set; }
+        public Attendee Attendee { get; set; }
+    }
+
+    public enum BadgeTier
+    {
+        Standard,
+        VIP
+    }
+}
