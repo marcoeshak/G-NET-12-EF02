5b034815845b2ce1233be857e5a30f11d4cf796a
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 01:00:52 +0300

    DbContext

---
+++ G-NET-12-EF02/DbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace G_NET_12_EF02
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Organizer> Organizers { get; set; }
+        public DbSet<OrganizerProfile> Profiles { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Organizer>()
+                .HasOne(o => o.Profile)
+                .WithOne(p => p.Organizer)
+                .HasForeignKey<OrganizerProfile>(p => p.OrganizerId);
+
+            modelBuilder.Entity<Attendee>()
+                .OwnsOne(a => a.Address);
+
+            modelBuilder.Entity<Attendee>()
+                .HasOne(a => a.Badge)
+                .WithOne(b => b.Attendee)
+                .HasForeignKey<Badge>(b => b.AttendeeId);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
