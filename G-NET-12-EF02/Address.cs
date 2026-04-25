d8cfe6e22cb52d40251978c3c9d3d68e09d6c505
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 00:25:58 +0300

    Address

---
+++ G-NET-12-EF02/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
