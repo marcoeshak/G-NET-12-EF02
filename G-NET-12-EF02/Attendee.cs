75b0b6d97a7e532b6801501cfef7db98be826bb7
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 00:24:59 +0300

    Attendee

---
+++ G-NET-12-EF02/Attendee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Net;
+
+    public class Attendee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public  Address Address { get; set; }
+
+        public  Badge Badge { get; set; }
+
+        public ICollection<Registration> Registrations { get; set; }
+    }
+}
