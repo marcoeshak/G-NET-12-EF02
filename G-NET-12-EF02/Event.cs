7be8e983ecac9f06d3b0a5a1d3c19327fc21c73e
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 00:16:34 +0300

    Event

---
+++ G-NET-12-EF02/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public int MaxAttendees { get; set; }
+
+        public int OrganizerId { get; set; }
+        public Organizer Organizer { get; set; }
+
+        public int? ParentEventId { get; set; }
+        public Event ParentEvent { get; set; }
+        public ICollection<Event> Sessions { get; set; }
+
+        public ICollection<Registration> Registrations { get; set; }
+    }
+}
