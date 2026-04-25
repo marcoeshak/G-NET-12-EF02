d127e740137019e944bcf18feea796a46d328474
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 00:22:10 +0300

    Event Configuration

---
+++ G-NET-12-EF02/Event Configuration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+
+
+    public class EventConfig : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.HasOne(e => e.ParentEvent)
+                .WithMany(e => e.Sessions)
+                .HasForeignKey(e => e.ParentEventId);
+
+            // Shadow Properties
+            builder.Property<DateTime>("CreatedAt")
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.Property<DateTime>("UpdatedAt")
+                .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
