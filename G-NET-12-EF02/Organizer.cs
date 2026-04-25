515343185a1ea12b53c7ea26edd1409028c8501c
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 00:11:57 +0300

    Organizer class

---
+++ G-NET-12-EF02/Organizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Organizer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public string? CompanyName { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public Organizer Profile Profile { get; set; }
+
+        public ICollection<Event> Events { get; set; }
+    }
+}
