7fc91909866a4332f1705ca9f6b0272c8a0458ec
marco <marco@DESKTOP-GCUIOA1>
2026-04-26 00:52:01 +0300

    Registration

---
+++ G-NET-12-EF02/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_12_EF02
+{
+    public class Registration
+    {
+        public int AttendeeId { get; set; }
+        public Attendee Attendee { get; set; }
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public string? Note { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
